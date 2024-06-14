2674e6224a0cc4a6588cd4862d6ea1042a7b6ef8
Lunar <133494114+LunarCliient@users.noreply.github.com>
2024-06-14 19:40:05 +0200

    Update and rename .gitignore to main.lua

---
+++ main.lua
@@ -0,0 +1,7 @@
+if game.id == 142823291 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LunarCliient/Orbit-MM2/main/main.lua"))()
+elseif game.id == 621129760 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LunarCliient/Orbit-Kat/main/.main.lua"))()
+elseif game.id == 12355337193 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LunarCliient/Orbit-MvsSh/main/.main.lua"))()
+end
